9843cc9253eca543b972ab3f943da4dde1a9b77a
Bradley Wilson <bluedirtbike6@gmail.com>
2020-06-08 11:13:19 -0500

    add schema and sql files, and populated the database.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Mushroom Swiss');
+INSERT INTO burgers (name) VALUES ('Baconator');
+INSERT INTO burgers (name, devoured) VALUES ('Double Bacon Butterburger Deluxe', true);
